77ce8c1b8b214ecf31048ab5c3d318bf103dab85
Eszenn <159959736+Eszenn@users.noreply.github.com>
2024-07-18 14:04:29 -0600

    Update to latest stable

--- hooks/Options.py
+++ hooks/Options.py
@@ -1,10 +1,17 @@
 # Object classes from AP that represent different types of options that you can create
-from Options import FreeText, NumericOption, Toggle, DefaultOnToggle, Choice, TextChoice, Range, SpecialRange
+from Options import FreeText, NumericOption, Toggle, DefaultOnToggle, Choice, TextChoice, Range, NamedRange
 
 # These helper methods allow you to determine if an option has been set, or what its value is, for any player in the multiworld
 from ..Helpers import is_option_enabled, get_option_value
 
 
+class IncludePaleReach(Toggle):
+    """Whether items / locations from the Pale Reach DLC should be included."""
+    display_name = "include_pale_reach"
+
+
+class IncludeAberrations(DefaultOnToggle):
+    """Whether aberrations should be included."""
 
 ####################################################################
 # NOTE: At the time that options are created, Manual has no concept of the multiworld or its own world. 
@@ -27,12 +34,14 @@ from ..Helpers import is_option_enabled, get_option_value
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 #
 class RelicShuffle(Toggle):
-    """Whether relics should be shuffled in to the item pool or not"""
+    """Whether relics should be shuffled into the item pool or placed at their vanilla locations."""
     display_name = "Enable relic shuffle?"
 
 
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict) -> dict:
+    options["include_pale_reach"] = IncludePaleReach
+    options["include_aberrations"] = IncludeAberrations
     options["shuffle_relics"] = RelicShuffle
     return options
 
